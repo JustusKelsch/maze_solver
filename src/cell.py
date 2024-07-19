c433b27ce823039c5dab0c9589f5ea394c4afe94
Justus <33078472+JustusKelsch@users.noreply.github.com>
2024-07-18 22:11:57 -0600

    Added the ability to draw a line between the center of 2 cells.

--- src/cell.py
+++ src/cell.py
@@ -33,3 +33,20 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+            
+    
+    def draw_move(self, to_cell, undo=False):
+        color = "red"
+        if undo:
+            color = "gray"
+
+        half_length = abs(self._x2 - self._x1) // 2
+        x_center = half_length + self._x1
+        y_center = half_length + self._y1
+
+        half_length2 = abs(to_cell._x2 - to_cell._x1) // 2
+        x_center2 = half_length2 + to_cell._x1
+        y_center2 = half_length2 + to_cell._y1
+
+        line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
+        self._win.draw_line(line, color)
--- src/main.py
+++ src/main.py
@@ -9,16 +9,19 @@ def Main():
 
     cell_no_left = Cell(win)
     cell_no_left.has_left_wall = False
-    cell_no_left.draw(200, 200, 250, 250)
+    cell_no_left.draw(225, 225, 275, 275)
     cell_no_right = Cell(win)
     cell_no_right.has_right_wall = False
-    cell_no_right.draw(300, 300, 350, 350)
-    cell_no_top = Cell(win)
-    cell_no_top.has_top_wall = False
-    cell_no_top.draw(400, 400, 450, 450)
-    cell_no_bottom = Cell(win)
-    cell_no_bottom.has_bottom_wall = False
-    cell_no_bottom.draw(500, 500, 550, 550)
+    cell_no_right.draw(180, 230, 230, 280)
+    cell_no_right.draw_move(cell_no_left)
+
+
+    # cell_no_top = Cell(win)
+    # cell_no_top.has_top_wall = False
+    # cell_no_top.draw(400, 400, 450, 450)
+    # cell_no_bottom = Cell(win)
+    # cell_no_bottom.has_bottom_wall = False
+    # cell_no_bottom.draw(500, 500, 550, 550)
 
     win.wait_for_close()
 
