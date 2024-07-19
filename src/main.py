c326fead0d59645d89d3235b93700f7d9e64e05a
Justus <33078472+JustusKelsch@users.noreply.github.com>
2024-07-18 23:39:36 -0600

    Added the maze class to generate a grid to be the ground work of the maze.

--- src/main.py
+++ src/main.py
@@ -1,21 +1,24 @@
 from window import Window, Line, Point
 from cell import Cell
+from maze import Maze
 
 def Main():
 
     win = Window(800, 600)
-    cell = Cell(win)
-    cell.draw(100, 100, 150, 150)
+    maze = Maze(50, 50, 10, 10, 50, 50, win)
 
-    cell_no_left = Cell(win)
-    cell_no_left.has_left_wall = False
-    cell_no_left.draw(225, 225, 275, 275)
-    cell_no_right = Cell(win)
-    cell_no_right.has_right_wall = False
-    cell_no_right.draw(180, 230, 230, 280)
-    cell_no_right.draw_move(cell_no_left)
+    ## Cell Tests
+    
+    # cell = Cell(win)
+    # cell.draw(0, 0, 50, 50)
 
-
+    # cell_no_left = Cell(win)
+    # cell_no_left.has_left_wall = False
+    # cell_no_left.draw(225, 225, 275, 275)
+    # cell_no_right = Cell(win)
+    # cell_no_right.has_right_wall = False
+    # cell_no_right.draw(180, 230, 230, 280)
+    # cell_no_right.draw_move(cell_no_left)
     # cell_no_top = Cell(win)
     # cell_no_top.has_top_wall = False
     # cell_no_top.draw(400, 400, 450, 450)
---
+++ src/maze.py
@@ -0,0 +1,68 @@
+from time import sleep
+from cell import Cell
+
+
+class Maze:
+
+    def __init__(
+            self,
+            x1,
+            y1,
+            num_rows,
+            num_cols,
+            cell_size_x,
+            cell_size_y,
+            win,
+    ):
+        self._cells = []
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
+        self._create_cells()
+
+    def _create_cells(self):
+
+        ## Creates the correct amount of Cells
+        ## However the cells are copies of the same Cell
+
+        # self._cells = [
+        #     [Cell(self._win)] * self._num_rows
+        # ] * self._num_cols
+
+        # for i in range(len(self._cells)):
+        #     print(self._cells[i])
+        #     for j in range(len(self._cells[i])):
+        #         self._draw_cell(i, j)
+
+        for i in range(self._num_cols):
+            cols = []
+            for j in range(self._num_rows):
+                cols.append(Cell(self._win))
+            self._cells.append(cols)
+
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._draw_cell(i, j)
+
+
+    def _draw_cell(self, i, j):
+        if self._win is None:
+            return
+        x1 = self._x1 + i * self._cell_size_x
+        x2 = x1 + self._cell_size_x
+        y1 = self._y1 + j * self._cell_size_y
+        y2 = y1 + self._cell_size_y
+
+        self._cells[i][j].draw(x1, y1, x2, y2)
+        self._animate()
+
+    def _animate(self):
+        if self._win is None:
+            return
+        self._win.redraw()
+        sleep(0.05)
+
